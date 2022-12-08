ad1baf3b9471c2e8dea5bf8d6091cd5f92232774
Narendra Bhojanala <nbhojanala@delaplex.com>
2022-12-08 19:15:32 +0530

    Initial Commit

---
+++ src/main/java/com/scb/FileReadOperation/FileReadOperationApplication.java
@@ -0,0 +1,13 @@
+package com.scb.FileReadOperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileReadOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileReadOperationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scb/FileReadOperation/FileReadOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scb.FileReadOperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileReadOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
